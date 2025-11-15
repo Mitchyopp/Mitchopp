8bb72b3ae407180c38ffd6c54ede6ab5e3a5dace
Mitchy <MitchoppDEV@gmail.com>
2025-11-15 19:29:14 +0000

    added some styling to my name and margin

--- src/About.jsx
+++ src/About.jsx
@@ -3,11 +3,13 @@ import './About.css'
 function About({ pfp }) {
   return (
     <>
-      <img src={pfp} alt="My profile picture" class="avatar" />
-      <h1 class="about-title">About me!</h1>
-      <p class="about-description">
-        Hey! I'm Mitch, I'm 19 and a massive tech nerd, i love Linux, Japanese and more.. Add some more stuff here later..
-      </p>
+      <section class="about">
+        <img src={pfp} alt="My profile picture" class="avatar" />
+        <h1 class="about-title">About me!</h1>
+        <p class="about-description">
+          Hey! I'm <span class="name">Mitch</span>, I'm 19 and a massive tech nerd, i love Linux, Japanese and more.. Add some more stuff here later..
+        </p>
+      </section>
     </>
   );
 }
